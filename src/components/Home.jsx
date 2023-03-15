a954a462de3530ec60c115f935838c2744e4cae4
FedeDiaz31 <fede.diaz1231@gmail.com>
2023-03-15 14:36:50 -0300

    Endpoints create Tweets & delete Tweets

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import React from "react";
 import { useSelector } from "react-redux";
 
 function Home() {
@@ -19,11 +20,31 @@ function Home() {
     getUser();
   }, []);
 
+  /////////////////////////////////
+  const [tweets, setTweets] = React.useState([]);
+
+  useEffect(() => {
+    const getTweets = async () => {
+      const response = await axios({
+        // headers: { Authorization: `Bearer ${token}` },
+        method: "get",
+        url: "http://localhost:8000/tweets",
+      });
+      setTweets(response.data);
+    };
+    getTweets();
+  }, []);
+
+  console.log(tweets);
+
   return (
     <>
       <h2>
         {user.firstname} {user.lastname}
       </h2>
+      {tweets.map(
+        (tweet) => tweet.author._id === user._id && <p>{tweet.content}</p>
+      )}
     </>
   );
 }
