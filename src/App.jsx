46f5855986afc74909599b04d2d71c4c88ac0496
lemilegram <97851079+lemilegram@users.noreply.github.com>
2023-03-15 16:48:51 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import "./App.css";
 import Home from "./components/Home/Home";
-import Login from "./components/Login/Login";
+import Login from "./components/AuthuserPages/Login/Login";
 import React from "react";
-import { useSelector } from "react-redux"
+import { useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const token = useSelector((state) => state.token);
-  console.log(token)
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/Login" element={<Login />} />
-        <Route path="/" element={token ? <Home />:<Login/>} />
-       
-      </Routes>
-    </div>
-  );
+	const token = useSelector((state) => state.token);
+	console.log(token);
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/Login" element={<Login />} />
+				<Route path="/" element={token ? <Home /> : <Login />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
