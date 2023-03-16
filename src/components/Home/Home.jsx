50f786491d1296816da51738c252640364d14d22
FedeDiaz31 <fede.diaz1231@gmail.com>
2023-03-16 14:17:15 -0300

    Cambios Profile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,67 +10,67 @@ import WhoToFollow from "../SideCards/WhoToFollow";
 import WhatsHappening from "../SideCards/WhatsHappening";
 
 function Home() {
-	const token = useSelector((state) => state.token);
+  const token = useSelector((state) => state.token);
 
-	const [user, setUser] = useState([]);
+  const [user, setUser] = useState([]);
 
-	useEffect(() => {
-		const getUser = async () => {
-			const response = await axios({
-				headers: { Authorization: `Bearer ${token}` },
-				method: "get",
-				url: "http://localhost:8000",
-			});
-			setUser(response.data);
-		};
-		getUser();
-	}, []);
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios({
+        headers: { Authorization: `Bearer ${token}` },
+        method: "get",
+        url: "http://localhost:8000",
+      });
+      setUser(response.data);
+    };
+    getUser();
+  }, []);
 
-	/////////////////////////////////
-	const [tweets, setTweets] = React.useState([]);
+  /////////////////////////////////
+  const [tweets, setTweets] = React.useState([]);
 
-	useEffect(() => {
-		const getTweets = async () => {
-			const response = await axios({
-				// headers: { Authorization: `Bearer ${token}` },
-				method: "get",
-				url: "http://localhost:8000/tweets",
-			});
-			setTweets(response.data);
-		};
-		getTweets();
-	}, []);
+  useEffect(() => {
+    const getTweets = async () => {
+      const response = await axios({
+        // headers: { Authorization: `Bearer ${token}` },
+        method: "get",
+        url: "http://localhost:8000/tweets",
+      });
+      setTweets(response.data);
+    };
+    getTweets();
+  }, []);
 
-	return (
-		<>
-			<div className="container-fluid">
-				<div className="row mt-4">
-					<div className="d-none d-xl-block col-lg-1"></div>
-					<div className="col-2 border-end">
-						<Navbar></Navbar>
-					</div>
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row mt-4">
+          <div className="d-none d-xl-block col-lg-1"></div>
+          <div className="col-2 border-end">
+            <Navbar></Navbar>
+          </div>
 
-					<div id="main-home-container" className="col-9 col-lg-5">
-						{tweets.map(
-							(tweet) =>
-								tweet.author._id === user._id && (
-									<Tweet
-										likes={tweet.likes.length}
-										content={tweet.content}
-										user={tweet.author.username}
-									/>
-								)
-						)}
-					</div>
+          <div id="main-home-container" className="col-9 col-lg-5">
+            {tweets.map(
+              (tweet) =>
+                tweet.author._id === user._id && (
+                  <Tweet
+                    likes={tweet.likes.length}
+                    content={tweet.content}
+                    user={tweet.author.username}
+                  />
+                )
+            )}
+          </div>
 
-					<div className="d-none d-lg-block col-lg-2 border-start">
-						<WhatsHappening /> <br /> <WhoToFollow />
-					</div>
-					<div className="d-none d-xl-block col-2"></div>
-				</div>
-			</div>
-		</>
-	);
+          <div className="d-none d-lg-block col-lg-2 border-start">
+            <WhatsHappening /> <br /> <WhoToFollow />
+          </div>
+          <div className="d-none d-xl-block col-2"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
