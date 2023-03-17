5e64381bff9f727cfe6371d7011d0ecfdc8f14df
Juan Manuel Ottado <juanmanuelottado@gmail.com>
2023-03-17 17:52:49 -0300

    tofollow

--- src/components/followAndFollowing/ToFollow.jsx
+++ src/components/followAndFollowing/ToFollow.jsx
@@ -1,32 +1,25 @@
-import React from 'react'
+import React from "react";
+import FollowBtn from "../SmallComponents/FollowBtn";
 
-function ToFollow({user}) {
+function ToFollow({ user }) {
   return (
     <div class="p-2 container w-100">
-    <div class="my-3">
+      <div class="my-3">
         <div class="col">
-           <div class="d-flex align-items-center ms-1">
-             <img class="me-3 p-1" src="/img/avatarSmall.svg" alt="" srcset="" />
-                <div>
-                  <p class="mb-0 fw-bold">{user.username}</p>
-                  <p class="mb-0 font-grey">@{user.username}</p>
-                </div>
-                <div class="ms-auto me-2">
-                  <form id="follow-form" action="/usuarios/following/unfollow" method="POST">
-                    
-                    <button type="submit" class="btn ms-auto me-1 rounded-pill btn-skyblue">Unfollow</button>
-                  </form>
-                </div>
+          <div class="d-flex align-items-center ms-1">
+            <img class="me-3 p-1" src="/img/avatarSmall.svg" alt="" srcset="" />
+            <div>
+              <p class="mb-0 fw-bold">{user.username}</p>
+              <p class="mb-0 font-grey">@{user.username}</p>
             </div>
-        </div>     
-    </div>      
-</div>
-  )
+            <div class="ms-auto me-2">
+                {<FollowBtn user={user}/>}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-
-
-export default ToFollow
+export default ToFollow;
