4954ce27f2ba2181bd278b24bfb45b81d739c86a
lemilegram <97851079+lemilegram@users.noreply.github.com>
2023-03-17 12:55:04 -0300

    Revert "Broken Heart"
    This reverts commit 842d395b79ed6bef0cd26c75449ec881b0316e9a.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,70 +10,71 @@ import WhatsHappening from "../SideCards/WhatsHappening";
 import WriteTweet from "../WriteTweet/WriteTweet";
 
 function Home() {
-	const token = useSelector((state) => state.token);
+  const token = useSelector((state) => state.token);
 
-	const [user, setUser] = useState([]);
-	const [idsFollowingsAndProfile, setIdsFollowingsAndProfile] =
-		React.useState([]);
+  const [user, setUser] = useState([]);
+  const [idsFollowingsAndProfile, setIdsFollowingsAndProfile] = React.useState(
+    []
+  );
 
-	useEffect(() => {
-		const getUser = async () => {
-			const response = await axios({
-				headers: { Authorization: `Bearer ${token}` },
-				method: "get",
-				url: "http://localhost:8000",
-			});
-			setUser(response.data);
-			setIdsFollowingsAndProfile([
-				...response.data.following,
-				response.data._id,
-			]);
-		};
-		getUser();
-	}, []);
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await axios({
+        headers: { Authorization: `Bearer ${token}` },
+        method: "get",
+        url: "http://localhost:8000",
+      });
+      setUser(response.data);
+      setIdsFollowingsAndProfile([
+        ...response.data.following,
+        response.data._id,
+      ]);
+    };
+    getUser();
+  }, []);
 
-	/////////////////////////////////
-	const [tweets, setTweets] = React.useState([]);
+  /////////////////////////////////
+  const [tweets, setTweets] = React.useState([]);
 
-	useEffect(() => {
-		const getTweets = async () => {
-			const response = await axios({
-				headers: { Authorization: `Bearer ${token}` },
-				method: "get",
-				url: "http://localhost:8000/tweets",
-			});
-			setTweets(response.data);
-		};
-		getTweets();
-	}, []);
+  useEffect(() => {
+    const getTweets = async () => {
+      const response = await axios({
+        headers: { Authorization: `Bearer ${token}` },
+        method: "get",
+        url: "http://localhost:8000/tweets",
+      });
+      setTweets(response.data);
+    };
+    getTweets();
+  }, []);
 
-	return (
-		<>
-			<div className="container-fluid">
-				<div className="row mt-4">
-					<div className="d-none d-xl-block col-lg-1"></div>
-					<div className="col-2 border-end">
-						<Navbar></Navbar>
-					</div>
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row mt-4">
+          <div className="d-none d-xl-block col-lg-1"></div>
+          <div className="col-2 border-end">
+            <Navbar></Navbar>
+          </div>
 
-					<div id="main-home-container" className="col-9 col-lg-5">
-						<WriteTweet></WriteTweet>
-						{tweets.map(
-							(tweet) =>
-								idsFollowingsAndProfile.includes(
-									tweet.author._id
-								) && <Tweet user={tweet.author} tweet={tweet} />
-						)}
-					</div>
+          <div id="main-home-container" className="col-9 col-lg-5">
+            <WriteTweet></WriteTweet>
+            {tweets.map(
+              (tweet) =>
+                idsFollowingsAndProfile.includes(tweet.author._id) && (
+                  <Tweet user={tweet.author} tweet={tweet} />
+                )
+            )}
+          </div>
 
-					<div className="d-none d-lg-block col-lg-2 border-start">
-						<WhatsHappening /> <br /> <WhoToFollow />
-					</div>
-					<div className="d-none d-xl-block col-2"></div>
-				</div>
-			</div>
-		</>
-	);
+          <div className="d-none d-lg-block col-lg-2 border-start">
+            <WhatsHappening /> <br /> <WhoToFollow />
+          </div>
+          <div className="d-none d-xl-block col-2"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
