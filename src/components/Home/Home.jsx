42127bff6f1eaa512afc6af1a276a117ed2068c3
lemilegram <97851079+lemilegram@users.noreply.github.com>
2023-03-17 14:53:41 -0300

    No tengo ni idea

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,58 +10,54 @@ import WhatsHappening from "../SideCards/WhatsHappening";
 import WriteTweet from "../WriteTweet/WriteTweet";
 
 function Home() {
-  const token = useSelector((state) => state.token);
-  const userDates = useSelector((state) => state.user);
+	const token = useSelector((state) => state.token);
+	const userDates = useSelector((state) => state.user);
 
-  const [idsFollowingsAndProfile, setIdsFollowingsAndProfile] = React.useState([
-    ...userDates.following,
-    userDates.id,
-  ]);
+	const [idsFollowingsAndProfile, setIdsFollowingsAndProfile] =
+		React.useState([...userDates.following, userDates.id]);
 
-  const [tweets, setTweets] = React.useState([]);
-  
-  useEffect(() => {
-    const getTweets = async () => {
-      const response = await axios({
-        headers: { Authorization: `Bearer ${token}` },
-        method: "get",
-        url: "http://localhost:8000/tweets",
-      });
+	const [tweets, setTweets] = React.useState([]);
 
-      setTweets(response.data);
-    };
-    getTweets();
-  }, []);
+	useEffect(() => {
+		const getTweets = async () => {
+			const response = await axios({
+				headers: { Authorization: `Bearer ${token}` },
+				method: "get",
+				url: "http://localhost:8000/tweets",
+			});
 
+			setTweets(response.data);
+		};
+		getTweets();
+	}, []);
 
-  
-  return (
-    <>
-      <div className="container-fluid">
-        <div className="row mt-4">
-          <div className="d-none d-xl-block col-lg-1"></div>
-          <div className="col-2 border-end">
-            <Navbar></Navbar>
-          </div>
+	return (
+		<>
+			<div className="container-fluid">
+				<div className="row mt-4">
+					<div className="d-none d-xl-block col-lg-1"></div>
+					<div className="col-2 border-end">
+						<Navbar></Navbar>
+					</div>
 
-          <div id="main-home-container" className="col-9 col-lg-5">
-            <WriteTweet tweets={tweets} setTweets={setTweets} />
-            {tweets.map(
-              (tweet) =>
-                idsFollowingsAndProfile.includes(tweet.author) && (
-                  <Tweet user={tweet.author} tweet={tweet} />
-                )
-            )}
-          </div>
+					<div id="main-home-container" className="col-9 col-lg-5">
+						<WriteTweet tweets={tweets} setTweets={setTweets} />
+						{tweets.map(
+							(tweet) =>
+								idsFollowingsAndProfile.includes(
+									tweet.author
+								) && <Tweet user={tweet.author} tweet={tweet} />
+						)}
+					</div>
 
-          <div className="d-none d-lg-block col-lg-2 border-start">
-            <WhatsHappening /> <br /> <WhoToFollow />
-          </div>
-          <div className="d-none d-xl-block col-2"></div>
-        </div>
-      </div>
-    </>
-  );
+					<div className="d-none d-lg-block col-lg-2 border-start">
+						<WhatsHappening /> <br /> <WhoToFollow />
+					</div>
+					<div className="d-none d-xl-block col-2"></div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Home;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,129 +9,139 @@ import FollowBtn from "../SmallComponents/FollowBtn";
 import { NavLink } from "react-router-dom";
 
 function Profile() {
-  const token = useSelector((state) => state.token);
-  const [profile, setProfile] = useState({ followers: [] });
-  const [tweets, setTweets] = useState([]);
-  const [followersNumber, setFollowersNumber] = useState(0);
-  const [followingNumber, setFollowingNumber] = useState(0);
-  const { id } = useParams();
-  const [user, setUser] = useState([]);
-  const[refresh, setRefresh]=useState(false);
+	const token = useSelector((state) => state.token);
+	const [profile, setProfile] = useState({ followers: [] });
+	const [tweets, setTweets] = useState([]);
+	const [followersNumber, setFollowersNumber] = useState(0);
+	const [followingNumber, setFollowingNumber] = useState(0);
+	const { id } = useParams();
+	const [user, setUser] = useState([]);
+	const [refresh, setRefresh] = useState(false);
 
-  useEffect(() => {
-    const getProfileData = async () => {
-      const response = await axios({
-        headers: { Authorization: `Bearer ${token}` },
-        method: "get",
-        url: `http://localhost:8000/users/${id}`,
-      });
-      setProfile(response.data.data.user);
-      setTweets(response.data.data.tweets);
-      setFollowersNumber(response.data.data.user.followers.length);
-      setFollowingNumber(response.data.data.user.following.length);
-    };
-    getProfileData();
-  }, [refresh]);
+	useEffect(() => {
+		const getProfileData = async () => {
+			const response = await axios({
+				headers: { Authorization: `Bearer ${token}` },
+				method: "get",
+				url: `http://localhost:8000/users/${id}`,
+			});
+			setProfile(response.data.data.user);
+			setTweets(response.data.data.tweets);
+			setFollowersNumber(response.data.data.user.followers.length);
+			setFollowingNumber(response.data.data.user.following.length);
+		};
+		getProfileData();
+	}, [refresh]);
 
-  useEffect(() => {
-    const getUser = async () => {
-      const response = await axios({
-        headers: { Authorization: `Bearer ${token}` },
-        method: "get",
-        url: "http://localhost:8000",
-      });
-      setUser(response.data);
-    };
-    getUser();
-  }, []);
+	useEffect(() => {
+		const getUser = async () => {
+			const response = await axios({
+				headers: { Authorization: `Bearer ${token}` },
+				method: "get",
+				url: "http://localhost:8000",
+			});
+			setUser(response.data);
+		};
+		getUser();
+	}, []);
 
-  function follow() {
-    const followUser = async () => {
-      const response = await axios({
-        headers: { Authorization: `Bearer ${token}` },
-        method: "post",
-        url: `http://localhost:8000/users/follow/${profile._id}`,
-      });
-    };
-    followUser();
-    setFollowersNumber(followersNumber + 1);
-    setRefresh(prev => !prev)
-  }
+	function follow() {
+		const followUser = async () => {
+			const response = await axios({
+				headers: { Authorization: `Bearer ${token}` },
+				method: "post",
+				url: `http://localhost:8000/users/follow/${profile._id}`,
+			});
+		};
+		followUser();
+		setFollowersNumber(followersNumber + 1);
+		setRefresh((prev) => !prev);
+	}
 
-  function unFollow() {
-    const unFollowUser = async () => {
-      const response = await axios({
-        headers: { Authorization: `Bearer ${token}` },
-        method: "delete",
-        url: `http://localhost:8000/users/follow/${profile._id}`,
-      });
-    };
-    unFollowUser();
-    setRefresh(prev => !prev)
-  }
+	function unFollow() {
+		const unFollowUser = async () => {
+			const response = await axios({
+				headers: { Authorization: `Bearer ${token}` },
+				method: "delete",
+				url: `http://localhost:8000/users/follow/${profile._id}`,
+			});
+		};
+		unFollowUser();
+		setRefresh((prev) => !prev);
+	}
 
-  console.log(tweets);
+	console.log(tweets);
 
-  return (
-    <>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="d-none d-xl-block col-lg-1"></div>
-          <div className="col-2 border-end">
-            <Navbar></Navbar>
-          </div>
-          <div className="col-9 col-lg-5 px-0">
-            <div id="blue-div">
-              <img src={profile.photoPortada} alt="" />
-              <img
-                src={profile.photoProfile}
-                alt="foto-perfil"
-                id="profile-Photo"
-              />
-              {user._id === profile._id ? <></>:
-              profile.followers.includes(user._id) ? (
-                <button onClick={() => unFollow()} id="unfollow-btn">
-                  Unfollow
-                </button>
-              ) : (
-                <button onClick={() => follow()} id="follow-btn">
-                  Follow
-                </button>
-              )
-              }
-            </div>
-            <div id="white-div">
-              <div>
-                <h5>
-                  {profile.firstname} {profile.lastname}
-                </h5>
-                <p>@{profile.username}</p>
-                <span>{profile.bio}</span>
-              </div>
-              <div>
-                <NavLink to={`following `}>
-                  <span>{followingNumber}</span>
-                  <span className="gray-letter ms-1 me-2">Following</span>
-                </NavLink>
-                <NavLink to={`followers `}>
-                  <span>{followersNumber}</span>
-                  <span className="gray-letter ms-1 me-2">Followers</span>
-                </NavLink>
-              </div>
-            </div>
-            <div className="tweet-container">
-              {/* {tweets.map(
+	return (
+		<>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="d-none d-xl-block col-lg-1"></div>
+					<div className="col-2 border-end">
+						<Navbar></Navbar>
+					</div>
+					<div className="col-9 col-lg-5 px-0">
+						<div id="blue-div">
+							<img src={profile.photoPortada} alt="" />
+							<img
+								src={profile.photoProfile}
+								alt="foto-perfil"
+								id="profile-Photo"
+							/>
+							{user._id === profile._id ? (
+								<></>
+							) : profile.followers.includes(user._id) ? (
+								<button
+									onClick={() => unFollow()}
+									id="unfollow-btn"
+								>
+									Unfollow
+								</button>
+							) : (
+								<button
+									onClick={() => follow()}
+									id="follow-btn"
+								>
+									Follow
+								</button>
+							)}
+						</div>
+						<div id="white-div">
+							<div>
+								<h5>
+									{profile.firstname} {profile.lastname}
+								</h5>
+								<p>@{profile.username}</p>
+								<span>{profile.bio}</span>
+							</div>
+							<div>
+								<NavLink to={`following `}>
+									<span>{followingNumber}</span>
+									<span className="gray-letter ms-1 me-2">
+										Following
+									</span>
+								</NavLink>
+								<NavLink to={`followers `}>
+									<span>{followersNumber}</span>
+									<span className="gray-letter ms-1 me-2">
+										Followers
+									</span>
+								</NavLink>
+							</div>
+						</div>
+						<div className="tweet-container">
+							{/* {tweets.map(
                 (tweet) =>
                   tweet.includes(tweet.author) && (
                     <Tweet tweet={tweet} user={tweet.author} />
                   )
               )} */}
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Profile;
