95d0f836cef7636780646a7bec3c351aec8b3b43
FedeDiaz31 <fede.diaz1231@gmail.com>
2023-03-20 15:07:03 -0300

    actualizacion en Follow y Following

--- src/components/SideCards/WhoToFollow.jsx
+++ src/components/SideCards/WhoToFollow.jsx
@@ -5,35 +5,35 @@ import axios from "axios";
 import { motion } from "framer-motion";
 
 function WhoToFollow({ update }) {
-	const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-	useEffect(() => {
-		const getUsers = async () => {
-			const response = await axios({
-				method: "get",
-				url: "http://localhost:8000/users",
-			}).then((data) => setUsers(data.data.user));
-		};
-		getUsers();
-	}, []);
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await axios({
+        method: "get",
+        url: "http://localhost:8000/users",
+      }).then((data) => setUsers(data.data.user));
+    };
+    getUsers();
+  }, []);
 
-	return (
-		<motion.div
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ delay: 0.2, ease: "linear" }}
-			className="card border-0 px-2 card-css"
-		>
-			<ul className="list-group list-group-flush container px-0">
-				<li className="p-3">
-					<h4 className="fw-bolder">Who to follow</h4>
-				</li>
-				{users.slice(0, 5).map((user) => (
-					<UserToFollow user={user} update={update} />
-				))}
-			</ul>
-		</motion.div>
-	);
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.2, ease: "linear" }}
+      className="card border-0 px-2 card-css"
+    >
+      <ul className="list-group list-group-flush container px-0">
+        <li className="p-3">
+          <h4 className="fw-bolder">Who to follow</h4>
+        </li>
+        {users.slice(0, 5).map((user) => (
+          <UserToFollow user={user} update={update} updateFollows={update} />
+        ))}
+      </ul>
+    </motion.div>
+  );
 }
 
 export default WhoToFollow;
--- src/components/SmallComponents/UserToFollow.jsx
+++ src/components/SmallComponents/UserToFollow.jsx
@@ -1,44 +1,43 @@
 import { Link, useNavigate } from "react-router-dom";
 import FollowBtn from "../SmallComponents/FollowBtn";
 
-function UserToFollow({ user, update }) {
-	const updateProfile = () => {
-		update();
-	};
+function UserToFollow({ user, update, updateFollows }) {
+  const updateProfile = () => {
+    update();
+  };
+  const updateFollowings = () => {
+    updateFollows();
+  };
 
-	return (
-		<li className="pb-2 ps-2">
-			<div className="container">
-				<div className="d-flex align-items-center row">
-					<div className="col-2 pe-5">
-						<img
-							className="tiny-pic-user"
-							src={user.photoProfile}
-							alt=""
-						/>
-					</div>
-					<div className="col-5 ps-0">
-						<Link
-							to={"/users/" + user._id}
-							href={"/users/" + user.username}
-							className="text-truncate fw-bold small text-dark text-decoration-none"
-							onClick={updateProfile}
-						>
-							{user.username}
-						</Link>
+  return (
+    <li className="pb-2 ps-2">
+      <div className="container">
+        <div className="d-flex align-items-center row">
+          <div className="col-2 pe-5">
+            <img className="tiny-pic-user" src={user.photoProfile} alt="" />
+          </div>
+          <div className="col-5 ps-0">
+            <Link
+              to={"/users/" + user._id}
+              href={"/users/" + user.username}
+              className="text-truncate fw-bold small text-dark text-decoration-none"
+              onClick={updateProfile}
+            >
+              {user.username}
+            </Link>
 
-						<p className="mb-0 font-grey text-truncate small">
-							@{user.username}
-						</p>
-					</div>
+            <p className="mb-0 font-grey text-truncate small">
+              @{user.username}
+            </p>
+          </div>
 
-					<div className="col-4 ps-auto d-flex justify-content-end flex-grow-1">
-						<FollowBtn user={user} />
-					</div>
-				</div>
-			</div>
-		</li>
-	);
+          <div className="col-4 ps-auto d-flex justify-content-end flex-grow-1">
+            <FollowBtn user={user} updateFollowingList={updateFollowings} />
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default UserToFollow;
